ab62eacb880c93100915956ed08743c49a6d848c
Geoff Fox <fox.geoffrey@gmail.com>
2021-01-21 17:35:18 -0800

    Add auth to top menu

--- src/app/ui/nav/nav.component.ts
+++ src/app/ui/nav/nav.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,15 +11,28 @@ import { map } from 'rxjs/operators';
 })
 export class NavComponent {
 
+  isAuth = false;
+  authSubscription: Subscription;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) { }
 
   logout() {
-    
+    this.authService.logout();
+  }
+
+  AuthSubscription() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }
